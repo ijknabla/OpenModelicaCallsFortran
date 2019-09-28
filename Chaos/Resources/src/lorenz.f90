59c72705ee320c70a5e9b8f1aea0ddbd1e517ef8
ijknabla <ijknabla@gmail.com>
2019-09-28 22:06:10 +0900

    Add Fortran 03 backend implementation

--- Chaos/Resources/src/lorenz.f90
+++ Chaos/Resources/src/lorenz.f90
@@ -13,3 +13,24 @@ subroutine get_derivative_f90( &
     der_z = +x*y - b*z
 
 end subroutine get_derivative_f90
+
+module lorenz
+    use iso_c_binding
+    implicit none
+contains
+
+    subroutine get_derivative( &
+        p, r, b, x, y, z,      &
+        der_x, der_y, der_z    &
+    ) bind(C, name="getDerivativeF03")
+        real(c_double),value :: p, r, b, x, y, z
+        real(c_double),intent(out) :: der_x, der_y, der_z
+
+        der_x = -p*x + p*y
+        der_y = -x*z + r*x - y
+        der_z = +x*y - b*z
+
+    end subroutine get_derivative
+
+end module lorenz
+
